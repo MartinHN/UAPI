e8a2a950298a068b1bdf1f58d75e2961c91e1b55
tinmar <martin.hermant@gmail.com>
2023-04-12 09:52:12 +0200

    init

---
+++ runTest.sh
@@ -0,0 +1,30 @@
+set -e
+
+pushd "genJSONSchema/test"
+node index.mjs
+popd #"genJSONSchema/test"
+
+pushd "cppGen"
+ts-node index.ts
+
+# pushd "examples/websocketComms/cpp/build"
+
+# # export EMSDK_QUIET=1
+# # source "/Users/tinmarbook/Dev/emsdk/emsdk_env.sh"
+# # emsdk activate tot
+
+# CMAKE_DEFS="-DCMAKE_TOOLCHAIN_FILE=/Users/tinmarbook/Dev/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Release "
+
+# cmake .. ${CMAKE_DEFS}
+# make -j4
+
+# # ./test
+# # ls -ldh test
+# # ls -ldh /tmp/lala.bin
+# popd #"test/cpp/build"
+
+pushd examples/websocketComms/websock/build
+cmake .. && make -j4
+./websowk
+popd
+popd # "cppGen"s
